d408cda0477175d0801b72b905825cf69b919969
Lewizard98 <55242367+Lewizard98@users.noreply.github.com>
2021-01-11 17:11:14 +0000

    Create exercise-5.cpp

---
+++ exercise-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  int inputAge;
+  int retirementAge;
+  int untilRetirement;
+  int retirementYear;
+  const int currentYear = 2021;
+
+  std::cout << "Please enter your current age: ";
+  std::cin >> inputAge;
+  std::cout << "At what age would you like to retire: ";
+  std::cin >> retirementAge;
+
+  untilRetirement = retirementAge - inputAge;
+  retirementYear = currentYear + untilRetirement;
+
+  std::cout << "The current year is " << currentYear << ", you can retire in " << retirementYear << " you have " << untilRetirement << " remaining";
+}
